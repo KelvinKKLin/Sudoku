07fc216f18e9e8a16bb2a560e5a69545063471b7
Kelvin Lin <kelvinkklin@gmail.com>
2016-09-05 12:07:46 -0400

    Updated Solver

--- Sudoku-Game/src/sudoku/Solver.java
+++ Sudoku-Game/src/sudoku/Solver.java
@@ -3,9 +3,50 @@ package sudoku;
 public class Solver {
 
 	private GridObject gridObject;
+	private int[][] possibleValues = new int[9][9];
 	
 	public Solver(GridObject g){
 		gridObject = g;
+		gridObject.resetGrid();
+		initPossibleValues();
+	}
+	
+	/**
+	 * This method returns the index of the first most constrained square (i, j)
+	 * @return	The index of the most constrained square (i, j)
+	 */
+	private int[] chooseNextSquare(){
+		int[][] grid = gridObject.getGrid();
+		int[] index = new int[2];
+		int minValue = 10;
+		for(int i = 0; i < possibleValues.length; i++){
+			for(int j = 0; j < possibleValues[0].length; j++){
+				if(grid[i][j] < minValue){
+					//TODO: Update variables
+				}
+			}
+		}
+		return index;
+	}
+	
+	private void initPossibleValues(){
+		int[][] grid = gridObject.getGrid();
+		for(int i = 0; i < grid.length; i++){
+			for(int j = 0; j < grid[0].length; j++){
+				if(grid[i][j] != 0){
+					possibleValues[i][j] = 10;
+				}
+			}
+		}
+	}
+	
+	private void printArray(int[][] array){
+		for(int i = 0; i < array.length; i++){
+			for(int j = 0; j < array[0].length; j++){
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 	
 	
